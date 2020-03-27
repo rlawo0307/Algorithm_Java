d79f653dbb88298722cf1d3240412bca22363adf
rlawo0307 <rlawo0307@naver.com>
2020-03-27 20:23:33 +0900

    Add files via upload

---
+++ 백준 DP/src/_11726.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class _11726 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n+1];
+		
+		for(int i=1; i<=n; i++)
+		{
+			if(i == 1) arr[i] = 1;
+			else if(i == 2) arr[i] = 2;
+			else
+				arr[i] = (arr[i-1]+arr[i-2])%10007;
+		}
+		System.out.println(arr[n]);
+	}
+}
