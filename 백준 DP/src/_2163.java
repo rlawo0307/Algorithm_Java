427db105916255033e83bb4836f41dfd21c4985d
rlawo0307 <rlawo0307@naver.com>
2020-04-07 13:28:17 +0900

    Add files via upload

---
+++ 백준 DP/src/_2163.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class _2163 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println((a-1)+(b-1)*a);
+	}
+}
